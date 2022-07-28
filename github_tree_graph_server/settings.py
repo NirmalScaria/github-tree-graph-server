acd4b5cbe34042f923683ac727f8aced7bf63344
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2022-07-28 23:36:02 +0530

    [UI] Home Page

--- github_tree_graph_server/settings.py
+++ github_tree_graph_server/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "github_tree_graph_server",
 ]
 
 MIDDLEWARE = [
--- github_tree_graph_server/urls.py
+++ github_tree_graph_server/urls.py
@@ -1,3 +1,4 @@
+from . import views
 """github_tree_graph_server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -14,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
